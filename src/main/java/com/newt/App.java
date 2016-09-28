33bc6714fe1142ca1be4059414301614f10c4ca1
Vasudha <vasudhad@newtglobal.chn.local>
2016-09-28 10:41:16 +0530

    1st commit

---
+++ src/main/java/com/newt/App.java
@@ -0,0 +1,13 @@
+package com.newt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "In Hyderabad" );
+    }
+}
